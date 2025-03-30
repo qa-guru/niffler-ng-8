592b56779b6a538a2724f48a8b4bd2aa1fa8db6f
Timofey050398 <Otdelprodajclock@yandex.ru>
2025-03-30 20:51:40 +0300

    Поправил приватный метод executeRequest

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
@@ -29,10 +29,11 @@ public class SpendApiClient {
 
   private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-  private <T> T executeRequest(Call<T> call) {
+  private <T> T executeRequest(Call<T> call, int successLastDigit) {
     try {
+      int successCode = 200+successLastDigit;
       Response<T> response = call.execute();
-      assertEquals(200, response.code());
+      assertEquals(successCode, response.code());
       return response.body();
     } catch (IOException e) {
       throw new AssertionError(e);
@@ -41,34 +42,34 @@ public class SpendApiClient {
 
   // Используем универсальный метод для всех операций
   public SpendJson addSpend(SpendJson spend) {
-    return executeRequest(spendApi.addSpend(spend));
+    return executeRequest(spendApi.addSpend(spend),1);
   }
 
   public SpendJson editSpend(SpendJson spend) {
-    return executeRequest(spendApi.editSpend(spend));
+    return executeRequest(spendApi.editSpend(spend),0);
   }
 
   public SpendJson getSpend(String id, String username) {
-    return executeRequest(spendApi.getSpend(id, username));
+    return executeRequest(spendApi.getSpend(id, username),0);
   }
 
   public List<SpendJson> getSpends(String username, CurrencyValues currencyValues, Date from, Date to) {
-    return executeRequest(spendApi.getSpends(username, currencyValues, from, to));
+    return executeRequest(spendApi.getSpends(username, currencyValues, from, to),0);
   }
 
   public Response<Void> deleteSpends(String username, List<String> ids) {
-    return executeRequest(spendApi.deleteSpends(username, ids));
+    return executeRequest(spendApi.deleteSpends(username, ids),2);
   }
 
   public List<CategoryJson> getCategories(String username, boolean excludeArchived) {
-    return executeRequest(spendApi.getCategories(username, excludeArchived));
+    return executeRequest(spendApi.getCategories(username, excludeArchived),0);
   }
 
   public CategoryJson addCategory(CategoryJson category) {
-    return executeRequest(spendApi.addCategory(category));
+    return executeRequest(spendApi.addCategory(category),0);
   }
 
   public CategoryJson updateCategory(CategoryJson category) {
-    return executeRequest(spendApi.updateCategory(category));
+    return executeRequest(spendApi.updateCategory(category),0);
   }
 }

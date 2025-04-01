e8e77f360088d9cf395a25a0dae4c5324811b052
Лесников Илья Алексеевич <ilesnikov@hq.icfed.com>
2025-04-01 13:29:23 +0300

    lesson_3_1 написал методы для отправки запросов к эндпоинтам категорий и трат

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -1,5 +1,6 @@
 package guru.qa.niffler.api;
 
+import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
 import retrofit2.http.*;
@@ -11,15 +12,15 @@ public interface SpendApi {
   @PATCH("internal/spends/edit")
   Call<SpendJson> editSpend(@Body SpendJson spend);
   @GET("internal/spends/{id}")
-  Call<SpendJson> getSpend();
+  Call<SpendJson> getSpend(@Path("id") String id);
   @GET("internal/spends/all")
   Call<SpendJson> getSpends();
   @DELETE("internal/spends/remove")
-  Call<SpendJson> deleteSpend();
+  Call<SpendJson> deleteSpend(@Query("ids") String ids);
   @POST("internal/categories/add")
-  Call<SpendJson> addCategorie();
-  @POST("internal/categories/update")
-  Call<SpendJson> updateCategorie();
+  Call<SpendJson> addCategory(@Body CategoryJson category);
+  @PATCH("internal/categories/update")
+  Call<SpendJson> updateCategory(@Body CategoryJson category);
   @GET("internal/categories/all")
   Call<SpendJson> getCategories();
 }

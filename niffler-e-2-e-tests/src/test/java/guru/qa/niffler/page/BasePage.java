80d35ceaf4db2bd63c11735e084ac4fd91e3d711
Timofey050398 <Otdelprodajclock@yandex.ru>
2025-04-02 14:22:59 +0300

    Правки по ревью + добавил метод, открывающий вкладку друзья

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/BasePage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/BasePage.java
@@ -3,19 +3,15 @@ package guru.qa.niffler.page;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
 import guru.qa.niffler.config.Config;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class BasePage {
     private static final SelenideElement avatarButton = $(byXpath("//header//button"));
@@ -28,38 +24,32 @@ public class BasePage {
         return (T) this;
     }
 
-    private void clickMenuButtonsExcludeLogout(int i){
+    private <T extends BasePage> T clickMenuButtonsExcludeLogout(int i){
         menuButtons.get(i).find(By.xpath("./a")).click();
+        return (T) this;
     }
 
     public ProfilePage openProfilePage(){
-        openMenu();
-        clickMenuButtonsExcludeLogout(0);
+        openMenu()
+                .clickMenuButtonsExcludeLogout(0);
         return new ProfilePage();
     }
 
+    public FriendsPage openFriendsPage(){
+        openMenu()
+                .clickMenuButtonsExcludeLogout(1);
+        return new FriendsPage();
+    }
+
     public <T extends BasePage> T assertRedirectToPage(Class<T> clazz, String... strings){
-        WebDriver driver = WebDriverRunner.getWebDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         boolean isAuthPage = clazz.equals(LoginPage.class) || clazz.equals(RegistrationPage.class);
         String basePage = isAuthPage? Config.getInstance().authUrl() : Config.getInstance().frontUrl();
-        String expectedUrl = basePage + Pages.getUrlByClass(clazz);
-        if(strings.length>0){
-            for (String param : strings){
-                expectedUrl+=param;
-            }
+        StringBuilder expectedUrl = new StringBuilder(basePage).append(Pages.getUrlByClass(clazz));
+        for (String param : strings) {
+            expectedUrl.append(param);
         }
-
+        webdriver().shouldHave(url(expectedUrl.toString()));
         try {
-
-            wait.until(ExpectedConditions.urlToBe(expectedUrl));
-        } catch (Exception e) {
-
-            throw new AssertionError("Не удалось попасть на страницу " + expectedUrl + " в течение 5 секунд." +
-                    "\n Текущий url: "+driver.getCurrentUrl());
-        }
-        try {
-
             return clazz.getDeclaredConstructor().newInstance();
         } catch (Exception e) {
             throw new AssertionError("Не удалось создать объект класса страницы.");

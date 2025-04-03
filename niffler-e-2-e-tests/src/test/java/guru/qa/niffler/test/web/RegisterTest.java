7664eaf1532bf8be0be16c6d5675838d6ad65f24
Alexander Kanishevskii <a.kan4988@gmail.com>
2025-04-03 23:04:38 +0300

    [HW-L03] feat: add FakerGenUtil.java

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/RegisterTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/RegisterTest.java
@@ -1,21 +1,22 @@
 package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
-import com.github.javafaker.Faker;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.RegisterPage;
 import org.junit.jupiter.api.Test;
 
+import static utils.FakerGenUtil.*;
+
 
 public class RegisterTest {
 
     private static final Config CFG = Config.getInstance();
-    public Faker faker = new Faker();
+
     @Test
     void shouldRegisterNewUser() {
-        String newUser = faker.name().fullName();
-        String password = faker.internet().password(3,10);
+        String newUser = genRandomName();
+        String password = genRandomPassword();
 
         RegisterPage registerPage =
                 Selenide.open(CFG.frontUrl(), LoginPage.class)

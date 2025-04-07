bfb9a128e4d8ba3c4fc95145ec3a9abf87395c4a
Константин <kocmoc007@gmail.com>
2025-04-07 13:38:58 +0300

    Lesson 3.3

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/TestMethodContextExtension.java
@@ -0,0 +1,25 @@
+package guru.qa.niffler.jupiter.extension;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class TestMethodContextExtension implements BeforeEachCallback, AfterEachCallback {
+
+    public static final ThreadLocal<ExtensionContext> STORE = new ThreadLocal<>();
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        STORE.set(context);
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        STORE.remove();
+    }
+
+    public static ExtensionContext context() {
+        return STORE.get();
+    }
+
+}

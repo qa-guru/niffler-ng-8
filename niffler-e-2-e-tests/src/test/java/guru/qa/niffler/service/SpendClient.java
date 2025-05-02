8c5b10614d125e3b6dbf6003630aa4507b2cb531
Timofey050398 <Otdelprodajclock@yandex.ru>
2025-05-02 20:00:36 +0300

    Домашнее задание 6.2 правки

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendClient.java
@@ -3,9 +3,13 @@ package guru.qa.niffler.service;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface SpendClient {
     SpendJson createSpend(SpendJson spend);
     void deleteSpend(SpendJson spend);
     CategoryJson createCategory(CategoryJson category);
     void deleteCategory(CategoryJson category);
+    Optional<SpendJson> findById(UUID spendId);
 }

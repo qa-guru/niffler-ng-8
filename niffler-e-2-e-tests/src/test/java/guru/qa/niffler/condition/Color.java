71f089ca515ce77d40734fa9bd071d6f4d6287e4
Константин <kocmoc007@gmail.com>
2025-06-15 12:59:38 +0300

    added colors

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/condition/Color.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/condition/Color.java
@@ -12,6 +12,13 @@ public enum Color {
 
     YELLOW("rgba(255, 183, 3, 1)"),
     GREEN("rgba(53, 173, 123, 1)"),
+    ORANGE("rgba(251, 133, 0, 1)"),
+    BLUE_100("rgba(41, 65, 204, 1)"),
+    AZURE("rgba(33, 158, 188, 1)"),
+    BLUE_200("rgba(22, 41, 149, 1)"),
+    RED("rgba(247, 89, 67, 1)"),
+    SKY_BLUE("rgba(99, 181, 226, 1)"),
+    PURPLE("rgba(148, 85, 198, 1)"),
     COLOR_NOT_FOUND("COLOR_NOT_FOUND");
 
     private static final List<Color> colors = Arrays.asList(values());

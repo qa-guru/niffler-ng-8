a6d28193183f899974d7b41887230c4f1b82a23d
Константин <kocmoc007@gmail.com>
2025-07-05 18:25:43 +0300

    11.1 Homework OAUTH 2.0 + OIDC. Базовые концепции. PKCE

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/model/OAuthTokenResponse.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/model/OAuthTokenResponse.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record OAuthTokenResponse(@JsonProperty("access_token") String accessToken,
                                  @JsonProperty("refresh_token") String refreshToken,
                                  @JsonProperty("token_type") String tokenType,
-                                 @JsonProperty("expires_in") Integer expiresIn, @JsonProperty("scope") String scope,
+                                 @JsonProperty("expires_in") Integer expiresIn,
+                                 @JsonProperty("scope") String scope,
                                  @JsonProperty("id_token") String idToken) {
 }

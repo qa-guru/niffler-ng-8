e267317b7c1fc89e69c2bdc433fdfb5eb51b4754
Berestov <berestov@protei.ru>
2025-07-29 21:59:13 +0300

    11.1 OAUTH 2.0 + OIDC. Home work

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/utils/OauthUtils.java
@@ -0,0 +1,27 @@
+package guru.qa.niffler.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class OauthUtils {
+
+
+    public static String generateCodeVerifier() {
+        byte[] randomBytes = new byte[32];
+        new SecureRandom().nextBytes(randomBytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes);
+    }
+
+    public static String generateCodeChallenge(String codeVerifier) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(codeVerifier.getBytes(StandardCharsets.UTF_8));
+            return Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Failed to generate code challenge", e);
+        }
+    }
+}

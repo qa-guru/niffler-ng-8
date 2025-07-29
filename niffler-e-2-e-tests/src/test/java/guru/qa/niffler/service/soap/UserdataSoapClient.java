6712a00fe56fa92c55f48724fad551da0cf523f8
Timofey050398 <Otdelprodajclock@yandex.ru>
2025-07-29 17:48:59 +0300

    homework 18.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/soap/UserdataSoapClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/soap/UserdataSoapClient.java
@@ -1,8 +1,8 @@
 package guru.qa.niffler.service.soap;
 
 
-import jaxb.userdata.CurrentUserRequest;
-import jaxb.userdata.UserResponse;
+import guru.qa.niffler.model.rest.UserJson;
+import jaxb.userdata.*;
 import guru.qa.niffler.api.UserdataSoapApi;
 import guru.qa.niffler.api.core.RestClient;
 import guru.qa.niffler.api.core.converter.SoapConverterFactory;
@@ -13,6 +13,8 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
+
+import static java.util.Objects.requireNonNull;
 
 @ParametersAreNonnullByDefault
 public class UserdataSoapClient extends RestClient {
@@ -29,5 +31,58 @@ public class UserdataSoapClient extends RestClient {
     @Step("Get current user info using SOAP")
     public UserResponse currentUser(CurrentUserRequest request) throws IOException {
         return userdataSoapApi.currentUser(request).execute().body();
+    }
+
+    @Step("Get friends info page for user using SOAP")
+    public UsersResponse allFriendsPage(UserJson userJson, int page, int size) throws IOException {
+        PageInfo pageInfo = new PageInfo();
+        pageInfo.setPage(page);
+        pageInfo.setSize(size);
+        FriendsPageRequest request = new FriendsPageRequest();
+        request.setUsername(userJson.username());
+        request.setPageInfo(pageInfo);
+        return requireNonNull(userdataSoapApi.friendsPage(request).execute().body());
+    }
+
+    @NotNull
+    @Step("Get friends info for user filtering by username using SOAP")
+    public UsersResponse allFriendsFilteringByUsername(UserJson userJson, String username) throws IOException {
+        FriendsRequest request = new FriendsRequest();
+        request.setUsername(userJson.username());
+        request.setSearchQuery(username);
+        return requireNonNull(userdataSoapApi.friends(request).execute().body());
+    }
+
+    @Step("Remove friend for user using SOAP")
+    public void removeFriend(UserJson userJson, String friendToBeRemoved) throws IOException {
+        RemoveFriendRequest request = new RemoveFriendRequest();
+        request.setUsername(userJson.username());
+        request.setFriendToBeRemoved(friendToBeRemoved);
+        userdataSoapApi.removeFriend(request).execute();
+    }
+
+    @Step("Accept income invitation for user using SOAP")
+    public void acceptIncomeInvitation(UserJson userJson, String friendToBeAddedUsername) throws IOException {
+        AcceptInvitationRequest request = new AcceptInvitationRequest();
+        request.setUsername(userJson.username());
+        request.setFriendToBeAdded(friendToBeAddedUsername);
+        userdataSoapApi.acceptInvitation(request).execute();
+    }
+
+    @Step("Decline income invitation for user using SOAP")
+    public void declineIncomeInvitation(UserJson userJson, String friendToBeDeclinedUsername) throws IOException {
+        DeclineInvitationRequest request = new DeclineInvitationRequest();
+        request.setUsername(userJson.username());
+        request.setInvitationToBeDeclined(friendToBeDeclinedUsername);
+        userdataSoapApi.declineInvitation(request).execute();
+    }
+
+    @NotNull
+    @Step("Send invitation using SOAP")
+    public UserResponse sendInvitation(UserJson userJson, String friendToBeRequestedUsername) throws IOException {
+        SendInvitationRequest request = new SendInvitationRequest();
+        request.setUsername(userJson.username());
+        request.setFriendToBeRequested(friendToBeRequestedUsername);
+        return requireNonNull(userdataSoapApi.sendInvitation(request).execute().body());
     }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/soap/SoapUsersTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/soap/SoapUsersTest.java
@@ -7,10 +7,14 @@ import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.SoapTest;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.service.soap.UserdataSoapClient;
-import org.junit.jupiter.api.Assertions;
+import jaxb.userdata.UsersResponse;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static jaxb.userdata.FriendshipStatus.INVITE_SENT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SoapTest
 public class SoapUsersTest {
@@ -23,9 +27,65 @@ public class SoapUsersTest {
         CurrentUserRequest request = new CurrentUserRequest();
         request.setUsername(user.username());
         UserResponse response = userdataSoapClient.currentUser(request);
-        Assertions.assertEquals(
+        assertEquals(
                 user.username(),
                 response.getUser().getUsername()
         );
+    }
+
+
+    @Test
+    @User(friends = 3)
+    void getAllFriendsPageForUserTest(UserJson userJson) throws IOException {
+        UsersResponse response = userdataSoapClient.allFriendsPage(userJson, 1, 10);
+        assertEquals(1, response.getTotalPages().intValue());
+        assertEquals(userJson.testData().friends().size(), response.getTotalElements().intValue());
+    }
+
+    @Test
+    @User(friends = 2)
+    void getAllFriendsForUserFilteringByUsernameTest(UserJson userJson) throws IOException {
+        String username = userJson.testData().friends().getFirst().username();
+        UsersResponse response = userdataSoapClient.allFriendsFilteringByUsername(userJson, username);
+        assertEquals(1, response.getUser().size());
+        assertTrue(userJson.testData().friends().stream()
+                .map(UserJson::username)
+                .toList().contains(response.getUser().getFirst().getUsername()));
+    }
+
+    @Test
+    @User(friends = 1)
+    void removeFriendTest(UserJson userJson) throws IOException {
+        String friendToBeRemovedUsername = userJson.testData().friends().getFirst().username();
+        userdataSoapClient.removeFriend(userJson, friendToBeRemovedUsername);
+        UsersResponse response = userdataSoapClient.allFriendsFilteringByUsername(userJson, friendToBeRemovedUsername);
+        assertEquals(0, response.getUser().size());
+    }
+
+    @Test
+    @User(incomeInvitations = 1)
+    void acceptIncomeInvitationTest(UserJson userJson) throws IOException {
+        String friendToBeAddedUsername = userJson.testData().incomeInvitations().getFirst().username();
+        userdataSoapClient.acceptIncomeInvitation(userJson, friendToBeAddedUsername);
+        UsersResponse response = userdataSoapClient.allFriendsFilteringByUsername(userJson, friendToBeAddedUsername);
+        assertEquals(1, response.getUser().size());
+        assertEquals(friendToBeAddedUsername, response.getUser().getFirst().getUsername());
+    }
+
+    @Test
+    @User(incomeInvitations = 1)
+    void declineIncomeInvitationTest(UserJson userJson) throws IOException {
+        String friendToBeDeclinedUsername = userJson.testData().incomeInvitations().getFirst().username();
+        userdataSoapClient.declineIncomeInvitation(userJson, friendToBeDeclinedUsername);
+        UsersResponse response = userdataSoapClient.allFriendsFilteringByUsername(userJson, friendToBeDeclinedUsername);
+        assertEquals(0, response.getUser().size());
+    }
+
+    @Test
+    @User
+    void sendInvitationTest(UserJson userJson) throws IOException {
+        String friendToBeRequestedUsername = "Ilya";
+        UserResponse response = userdataSoapClient.sendInvitation(userJson, friendToBeRequestedUsername);
+        assertEquals(INVITE_SENT, response.getUser().getFriendshipStatus());
     }
 }

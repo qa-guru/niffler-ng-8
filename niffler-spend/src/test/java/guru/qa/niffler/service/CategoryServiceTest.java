5be5b7e54df5bfd5c202e0160542922703464eba
Timofey050398 <Otdelprodajclock@yandex.ru>
2025-09-08 15:58:38 +0300

    lesson 13 mocks

--- niffler-spend/src/test/java/guru/qa/niffler/service/CategoryServiceTest.java
+++ niffler-spend/src/test/java/guru/qa/niffler/service/CategoryServiceTest.java
@@ -4,6 +4,7 @@ import guru.qa.niffler.data.CategoryEntity;
 import guru.qa.niffler.data.repository.CategoryRepository;
 import guru.qa.niffler.ex.CategoryNotFoundException;
 import guru.qa.niffler.ex.InvalidCategoryNameException;
+import guru.qa.niffler.ex.TooManyCategoriesException;
 import guru.qa.niffler.model.CategoryJson;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,17 +13,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
@@ -32,7 +33,7 @@ class CategoryServiceTest {
     final String username = "not_found";
     final UUID id = UUID.randomUUID();
 
-    Mockito.when(categoryRepository.findByUsernameAndId(eq(username), eq(id)))
+    when(categoryRepository.findByUsernameAndId(eq(username), eq(id)))
         .thenReturn(Optional.empty());
 
     CategoryService categoryService = new CategoryService(categoryRepository);
@@ -44,7 +45,7 @@ class CategoryServiceTest {
         true
     );
 
-    CategoryNotFoundException ex = Assertions.assertThrows(
+    CategoryNotFoundException ex = assertThrows(
         CategoryNotFoundException.class,
         () -> categoryService.update(categoryJson)
     );
@@ -56,12 +57,12 @@ class CategoryServiceTest {
 
   @ValueSource(strings = {"Archived", "ARCHIVED", "ArchIved"})
   @ParameterizedTest
-  void categoryNameArchivedShouldBeDenied(String catName, @Mock CategoryRepository categoryRepository) {
+  void categoryNameArchivedShouldBeDenied(String categoryName, @Mock CategoryRepository categoryRepository) {
     final String username = "duck";
     final UUID id = UUID.randomUUID();
     final CategoryEntity cat = new CategoryEntity();
 
-    Mockito.when(categoryRepository.findByUsernameAndId(eq(username), eq(id)))
+    when(categoryRepository.findByUsernameAndId(eq(username), eq(id)))
         .thenReturn(Optional.of(
             cat
         ));
@@ -70,17 +71,17 @@ class CategoryServiceTest {
 
     CategoryJson categoryJson = new CategoryJson(
         id,
-        catName,
+        categoryName,
         username,
         true
     );
 
-    InvalidCategoryNameException ex = Assertions.assertThrows(
+    InvalidCategoryNameException ex = assertThrows(
         InvalidCategoryNameException.class,
         () -> categoryService.update(categoryJson)
     );
     Assertions.assertEquals(
-        "Can`t add category with name: '" + catName + "'",
+        "Can`t add category with name: '" + categoryName + "'",
         ex.getMessage()
     );
   }
@@ -95,11 +96,11 @@ class CategoryServiceTest {
     cat.setName("Магазины");
     cat.setArchived(false);
 
-    Mockito.when(categoryRepository.findByUsernameAndId(eq(username), eq(id)))
+    when(categoryRepository.findByUsernameAndId(eq(username), eq(id)))
         .thenReturn(Optional.of(
             cat
         ));
-    Mockito.when(categoryRepository.save(any(CategoryEntity.class)))
+    when(categoryRepository.save(any(CategoryEntity.class)))
         .thenAnswer(invocation -> invocation.getArgument(0));
 
     CategoryService categoryService = new CategoryService(categoryRepository);
@@ -118,5 +119,124 @@ class CategoryServiceTest {
     assertEquals("duck", argumentCaptor.getValue().getUsername());
     assertTrue(argumentCaptor.getValue().isArchived());
     assertEquals(id, argumentCaptor.getValue().getId());
+  }
+
+  @Test
+  void getAllCategoriesIsFilteredByExcludeArchived(@Mock CategoryRepository categoryRepository) {
+    final String username = "timofey";
+    final CategoryEntity firstCategory = new CategoryEntity();
+    firstCategory.setId(UUID.randomUUID());
+    firstCategory.setUsername(username);
+    firstCategory.setName("Name");
+    firstCategory.setArchived(false);
+
+    final CategoryEntity secondCategory = new CategoryEntity();
+    secondCategory.setId(UUID.randomUUID());
+    secondCategory.setUsername(username);
+    secondCategory.setName("SecondName");
+    secondCategory.setArchived(false);
+
+    final CategoryEntity thirdCategory = new CategoryEntity();
+    thirdCategory.setId(UUID.randomUUID());
+    thirdCategory.setUsername(username);
+    thirdCategory.setName("ThirdName");
+    thirdCategory.setArchived(true);
+
+    when(categoryRepository.findAllByUsernameOrderByName(eq(username)))
+        .thenReturn(List.of(firstCategory, secondCategory, thirdCategory));
+
+    CategoryService categoryService = new CategoryService(categoryRepository);
+    List<CategoryJson> result = categoryService.getAllCategories(username, true);
+
+    assertEquals(2, result.size());
+    assertFalse(result.getFirst().archived());
+    assertFalse(result.getLast().archived());
+  }
+
+  @Test
+  void updateCategoryShouldThrowTooManyCategoriesExceptionIfMaxCategoriesSizeIsExceeded(@Mock CategoryRepository categoryRepository) {
+    final String username = "timofey";
+    final UUID catId = UUID.randomUUID();
+    final String catName = "Name";
+    final long count = 8;
+
+    final CategoryEntity category = new CategoryEntity();
+    category.setId(catId);
+    category.setUsername(username);
+    category.setName(catName);
+    category.setArchived(true);
+
+    final CategoryJson catJson = new CategoryJson(
+        catId,
+        catName ,
+        username,
+        false
+    );
+
+    when(categoryRepository.findByUsernameAndId(eq(username), eq(catId)))
+        .thenReturn(Optional.of(category));
+    when(categoryRepository.countByUsernameAndArchived(eq(username), eq(false)))
+        .thenReturn(count);
+
+    CategoryService categoryService = new CategoryService(categoryRepository);
+
+    final TooManyCategoriesException exception = assertThrows(TooManyCategoriesException.class,
+        () -> categoryService.update(catJson));
+    assertEquals(
+        "Can`t unarchive category for user: '" + username + "'",
+        exception.getMessage()
+    );
+  }
+
+  @ValueSource(strings = {"Archived", "ARCHIVED", "ArchIved"})
+  @ParameterizedTest
+  void saveShouldThrowExceptionInCaseOfArchivedCategoryName(String categoryName, @Mock CategoryRepository categoryRepository) {
+    final String username = "timofey";
+    final UUID id = UUID.randomUUID();
+
+    CategoryService categoryService = new CategoryService(categoryRepository);
+
+    CategoryJson categoryJson = new CategoryJson(
+        id,
+        categoryName,
+        username,
+        true
+    );
+
+    InvalidCategoryNameException ex = assertThrows(
+        InvalidCategoryNameException.class,
+        () -> categoryService.save(categoryJson)
+    );
+    Assertions.assertEquals(
+        "Can`t add category with name: '" + categoryName + "'",
+        ex.getMessage()
+    );
+  }
+
+  @Test
+  void saveShouldThrowExceptionInCaseOfMaxCategoriesSizeIsExceeded(@Mock CategoryRepository categoryRepository) {
+    final String username = "timofey";
+    final UUID catId = UUID.randomUUID();
+    final String categoryName = "Name";
+    final long count = 8;
+
+    final CategoryJson categoryJson = new CategoryJson(
+        catId,
+        categoryName,
+        username,
+        false
+    );
+
+    when(categoryRepository.countByUsernameAndArchived(eq(username), eq(false)))
+        .thenReturn(count);
+
+    CategoryService categoryService = new CategoryService(categoryRepository);
+
+    final TooManyCategoriesException exception = assertThrows(TooManyCategoriesException.class,
+        () -> categoryService.save(categoryJson));
+    assertEquals(
+        "Can`t add over than 8 categories for user: '" + username + "'",
+        exception.getMessage()
+    );
   }
 }
--- niffler-userdata/src/test/java/guru/qa/niffler/service/UserServiceTest.java
+++ niffler-userdata/src/test/java/guru/qa/niffler/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import guru.qa.niffler.data.UserEntity;
 import guru.qa.niffler.data.projection.UserWithStatus;
 import guru.qa.niffler.data.repository.UserRepository;
 import guru.qa.niffler.ex.NotFoundException;
+import guru.qa.niffler.ex.SameUsernameException;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.model.UserJsonBulk;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,14 +16,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static guru.qa.niffler.data.FriendshipStatus.PENDING;
 import static guru.qa.niffler.model.FriendshipStatus.INVITE_SENT;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static guru.qa.niffler.service.UserService.DEFAULT_USER_CURRENCY;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -160,6 +165,243 @@ class UserServiceTest {
     assertEquals(thirdTestUserName, friend.username());
   }
 
+
+  @Test
+  void allUsersShouldReturnSmallPhotoAndIgnoreFullPhoto(@Mock UserRepository userRepository) {
+    String fullPhoto = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAACwVBMVEUanW4am2wBY0QYmWsMfFYBZEQAAQBHcEz+" +
+        "/v4AAAAZm20anW4bnW4IdFAanW4anW4BZUQbnm8bnm8anW4anG4VflganW4anW4Jd1Ibnm8EbEoDaUcbnW4anW4anW4bnm8anW4anW4" +
+        "anG4BZUQWlGcHKR0anG0anW4FbksanG4anW4FbEobnm4anW4anW4anW4anW4anW4Zm2wanG0ZmmwZm20BY0MZmmwanW4anG4anG0cnm9B" +
+        "roj0+vj9/v4hoXIAYkMfoHEMfFYLe1QWlGgTjWIQh14XlWgNf1gNf1gAYUIAYkIAYUIAYUIAYUIKeFMAYUIEakgAYUISimAAYUIAYEEAYUE" +
+        "AYUIAYUIAYUIBCwcanW4MSjQHLB8BBwUJOCcNTzcSbUwVe1YanW4AAwIPWj8Sakvj8+0LQC0ZmmwAY0MAY0MAYkNdupglonVovp9RtJFbuZg1" +
+        "qX8Zm2yAyK7f8er3+/p9x6yk2MUDaUi038+Fy7IHck6w3c0KeFMGb0wEakllvZ4IdVAGcE0Le1UKeVMbnm8AAAD///8AYUIanW4Zm20amGsCEAs" +
+        "GJhoMfFYZmmwOVj0EHBQYj2QBBgQSbEwYjGIam2wUd1MYkmYCDQkFHhUOVDsJdVEJNSUZlWgSbU0Zk2cVkmYABANqv6Hs9vKr28pTtpLn9fBeupn+/" +
+        "v46q4Mdn3D6/PsDaUcPhFwBZEQIc08AYkIYmWsVfliU0bsAAQALQS0ZlWkOUjkYjmMUeFSb1MB5xqoBCQYDFg8KPSsHKx4Zl2oYkGV3xakanG4NUDgcnnC" +
+        "OzrcIMiMzqH5IsYv1+vmHzLNauJbF5tq84tRUtpPX7eWg1sJDr4gOgVn6/fyCyrArpXlwwqWm2cfa7+goo3fB5Ng9rIUjoXQhoHOe1sLT7ONLso0OgVrt9/P9/v2" +
+        "+49XI59zW7eUKeVOYug0QAAAAhnRSTlP+/q/+/gX+AP7+/vYU/QO9kAb7sv7+EcH+helU03EZM/0BIQj+/vv3E+2o/rBEFZlayYYr4Aj9RPrrlv7+/v7+VP71Bf7Zs" +
+        "PXhfOkTkvUd/ob+OP58/uHz2bD+0P7+/v7+/v7P/v7+/v66r5CQ/v7+/v7+uv7+/v7+kv7+Hf44hpL+HYY4/hahmy4AAAMTSURBVEjHY2BHBqbmRsUmagwMbVCkZpJp" +
+        "ZG6KooQBiW1sZQlXCkdtDJZWxlg1KGlZMDBg08DQZqGlhKGBT5a7DQ/gluVD1SAmDDEMuw0gJCyGrEEumoGQBoZkOYQGHpk2IoAMD0yDID/cMDw2tDHwC0I1SDIQp4FBEq" +
+        "JBXgirC7oXTpvcgyokJA/SoCjehmkDV+K8XrZ2zri+iYeR7RNXBGrQVcDUMGMSZzsnBO2bw4jQoKAL1KCD4ZiuOdvakUDfTISUDjuDnjoDmg1dCRCzo/aDXAVkzp4PV6Kux2DA" +
+        "gK5hARtQFdsBUeY2kWkTZ4H0xiO8YcCgj+6gQ71AZ8zaDeXNPNjePmkLQlafwRDdhlSgmVNE4YI9fTE9SAFlyKCJbsNsoAU7kfjTUWQ1GRjQbJicAgz+Hdhjuo2BAT0VMLTNAPo4F" +
+        "ipivxybBjQwFeii7RDm40drV2NEEoYNiUAbMsDc6mccHA8JOykJqEEVzG3s4OBY4UjQSfMXt7cvXghirXna0bHqHkEnMagC4+EYWOQ5B0dHLkEnMaS1c87bBRZZBtSwhKCT2o6eXNAFYV" +
+        "3o6OhYSdhJcGS9lINjlQ2GDd04NawEhtJS9FDiYnBHtm/1OQT7/kugi26hu8idwRWhfU25re1yGPdBLUcHh90V9IhzZXBD8IChwnGmxB7MtnkBDCKOCoy05MbgibDu4lqgIzpWXD5y8+zdayD" +
+        "mpTsYYejJUOeC8HR+GdBUBLpdhJG8XVoYnD2QQqmgFElD1Q3M/ODhzMBez4wUrHnX0yGq7c4XWmNmIOZmYLlU44TiyOOn927dc2LJKazFp1MTqGytXERsYbyoAVwYa0hNaCMKTJDSgBT30rwixN" +
+        "ggwisNq4EEJLoIa+iSEIBXWaxMc7sJuad7LhMrolJUztJWwW+DijaTMnK1y5pj9oQLtwauq2bZrGgtAQevdetxuKt7/TovB8ymg7eP7wb/4M3oNmwO9t/g6+ONtXHiFxAWuilw4ysWln4oat0YuCk0" +
+        "LMAPR2sG6JWgkMiI8E44CI+IDAliRVECAE4WhZg/rX3CAAAAAElFTkSuQmCC";
+
+    when(userRepository.findByUsernameNot(eq(mainTestUserName)))
+        .thenReturn(getMockUsersMappingFromDb());
+
+    userService = new UserService(userRepository);
+
+    final List<UserJsonBulk> actualUsers = userService.allUsers(mainTestUserName, null);
+
+    assertEquals(new String(getMockUsersMappingFromDb().getFirst().photoSmall(), StandardCharsets.UTF_8),
+        actualUsers.getFirst().photoSmall());
+    assertNotEquals(fullPhoto, actualUsers.getLast().photoSmall());
+  }
+
+  @Test
+  void getCurrentUserShouldReturnDefaultUserJsonInCaseThereIsNoRecordInDb(@Mock UserRepository userRepository) {
+    UserJson defaultUserJson = new UserJson(
+        null,
+        notExistingUser,
+        null,
+        null,
+        null,
+        DEFAULT_USER_CURRENCY,
+        null,
+        null,
+        null
+    );
+    when(userRepository.findByUsername(eq(notExistingUser)))
+        .thenReturn(Optional.empty());
+
+    userService = new UserService(userRepository);
+
+    UserJson actualCurrentUser = userService.getCurrentUser(notExistingUser);
+    assertEquals(defaultUserJson.id(), actualCurrentUser.id());
+    assertEquals(defaultUserJson.firstname(), actualCurrentUser.firstname());
+    assertEquals(defaultUserJson.surname(), actualCurrentUser.surname());
+    assertEquals(defaultUserJson.fullname(), actualCurrentUser.fullname());
+    assertEquals(defaultUserJson.currency(), actualCurrentUser.currency());
+    assertEquals(defaultUserJson.photo(), actualCurrentUser.photo());
+    assertEquals(defaultUserJson.photoSmall(), actualCurrentUser.photoSmall());
+    assertEquals(defaultUserJson.friendshipStatus(), actualCurrentUser.friendshipStatus());
+  }
+
+  @Test
+  void allUsersShouldCallCorrectRepositoryMethodInCaseSearchQueryIsPassed(@Mock UserRepository userRepository) {
+    String searchQuery = "bars";
+    userService = new UserService(userRepository);
+    userService.allUsers(mainTestUserName, searchQuery);
+    verify(userRepository, times(1)).findByUsernameNot(eq(mainTestUserName), eq(searchQuery));
+  }
+
+  @Test
+  void allUsersPageableShouldCallCorrectRepositoryMethodInCaseSearchQueryIsPassed(@Mock UserRepository userRepository) {
+    String searchQuery = "bars";
+    PageRequest pageRequest = PageRequest.of(0, 10);
+    when(
+        userRepository.findByUsernameNot(
+            eq(mainTestUserName),
+            eq(searchQuery),
+            eq(pageRequest)
+        )
+    )
+        .thenReturn(new PageImpl<>(getMockUsersMappingFromDb(), pageRequest, 0));
+
+    UserService userService = new UserService(userRepository);
+    userService.allUsers(mainTestUserName, pageRequest, searchQuery);
+    verify(userRepository, times(1)).findByUsernameNot(
+        eq(mainTestUserName),
+        eq(searchQuery),
+        eq(pageRequest)
+    );
+  }
+
+  @Test
+  void friendsShouldCallCorrectRepositoryMethodInCaseSearchQueryIsPassed(@Mock UserRepository userRepository) {
+    String searchQuery = "emma";
+    when(userRepository.findByUsername(eq(mainTestUserName))).thenReturn(Optional.of(mainTestUser));
+    when(userRepository.findFriends(eq(mainTestUser), eq(searchQuery)))
+        .thenReturn(getMockUserFriendFromDb());
+
+    userService = new UserService(userRepository);
+    userService.friends(mainTestUserName, searchQuery);
+
+    verify(userRepository, times(1)).findFriends(eq(mainTestUser), eq(searchQuery));
+  }
+
+
+  @Test
+  void friendsPageableShouldCallCorrectRepositoryMethodInCaseSearchQueryIsPassed(@Mock UserRepository userRepository) {
+    String searchQuery = "emma";
+    PageRequest pageRequest = PageRequest.of(0, 10);
+
+    when(userRepository.findByUsername(eq(mainTestUserName))).thenReturn(Optional.of(mainTestUser));
+
+    when(
+        userRepository.findFriends(
+            eq(mainTestUser),
+            eq(searchQuery),
+            eq(pageRequest)
+        )
+    )
+        .thenReturn(new PageImpl<>(getMockUserFriendFromDb(), pageRequest, 0));
+
+    UserService userService = new UserService(userRepository);
+    userService.friends(mainTestUserName, pageRequest, searchQuery);
+    verify(userRepository, times(1)).findFriends(
+        eq(mainTestUser),
+        eq(searchQuery),
+        eq(pageRequest)
+    );
+  }
+
+  @Test
+  void shouldThrowExceptionInCaseFriendshipIsCreatedForSelfUser(@Mock UserRepository userRepository) {
+    userService = new UserService(userRepository);
+
+    final SameUsernameException exception = assertThrows(SameUsernameException.class,
+        () -> userService.createFriendshipRequest(mainTestUserName, mainTestUserName));
+    assertEquals(
+        "Can`t create friendship request for self user",
+        exception.getMessage()
+    );
+  }
+
+  @Test
+  void friendshipRequestShouldBeCreated(@Mock UserRepository userRepository) {
+    thirdTestUser.addInvitations(mainTestUser);
+    when(userRepository.findByUsername(eq(mainTestUserName))).thenReturn(Optional.of(mainTestUser));
+    when(userRepository.findByUsername(eq(thirdTestUserName))).thenReturn(Optional.of(thirdTestUser));
+    when(userRepository.save(mainTestUser))
+        .thenReturn(thirdTestUser);
+
+    UserService userService = new UserService(userRepository);
+    UserJson result = userService.createFriendshipRequest(mainTestUserName, thirdTestUserName);
+
+    assertEquals(thirdTestUser.getId(), result.id());
+    assertEquals(thirdTestUser.getUsername(), result.username());
+    assertEquals(thirdTestUser.getFirstname(), result.firstname());
+    assertEquals(thirdTestUser.getSurname(), result.surname());
+    assertEquals(thirdTestUser.getFullname(), result.fullname());
+    assertEquals(thirdTestUser.getCurrency(), result.currency());
+    assertEquals(bytesToString(thirdTestUser.getPhoto()), result.photo());
+    assertEquals(bytesToString(thirdTestUser.getPhotoSmall()), result.photoSmall());
+    assertEquals(INVITE_SENT, result.friendshipStatus());
+
+    verify(userRepository, times(1)).save(eq(mainTestUser));
+  }
+
+  @Test
+  void shouldThrowExceptionInCaseFriendshipIsDeclinedForSelfUser(@Mock UserRepository userRepository) {
+    userService = new UserService(userRepository);
+
+    final SameUsernameException exception = assertThrows(SameUsernameException.class,
+        () -> userService.declineFriendshipRequest(mainTestUserName, mainTestUserName));
+    assertEquals(
+        "Can`t decline friendship request for self user",
+        exception.getMessage()
+    );
+  }
+
+  @Test
+  void friendshipRequestShouldBeDeclined(@Mock UserRepository userRepository) {
+    mainTestUser.addInvitations(secondTestUser);
+    secondTestUser.addFriends(PENDING, mainTestUser);
+
+    when(userRepository.findByUsername(eq(mainTestUserName))).thenReturn(Optional.of(mainTestUser));
+    when(userRepository.findByUsername(eq(secondTestUserName))).thenReturn(Optional.of(secondTestUser));
+    when(userRepository.save(mainTestUser))
+        .thenReturn(secondTestUser);
+
+    UserService userService = new UserService(userRepository);
+    UserJson result = userService.declineFriendshipRequest(mainTestUserName, secondTestUserName);
+
+    assertEquals(secondTestUser.getId(), result.id());
+    assertEquals(secondTestUser.getUsername(), result.username());
+    assertEquals(secondTestUser.getFirstname(), result.firstname());
+    assertEquals(secondTestUser.getSurname(), result.surname());
+    assertEquals(secondTestUser.getFullname(), result.fullname());
+    assertEquals(secondTestUser.getCurrency(), result.currency());
+    assertEquals(bytesToString(secondTestUser.getPhoto()), result.photo());
+    assertEquals(bytesToString(secondTestUser.getPhotoSmall()), result.photoSmall());
+    assertNull(result.friendshipStatus());
+
+    verify(userRepository, times(1)).save(eq(mainTestUser));
+  }
+
+  @Test
+  void shouldThrowExceptionInCaseFriendshipIsRemovedForSelfUser(@Mock UserRepository userRepository) {
+    userService = new UserService(userRepository);
+
+    final SameUsernameException exception = assertThrows(SameUsernameException.class,
+        () -> userService.removeFriend(mainTestUserName, mainTestUserName));
+    assertEquals(
+        "Can`t remove friendship relation for self user",
+        exception.getMessage()
+    );
+  }
+
+  @Test
+  void friendshipRequestShouldBeRemoved(@Mock UserRepository userRepository) {
+    mainTestUser.addFriends(FriendshipStatus.ACCEPTED, thirdTestUser);
+    when(userRepository.findByUsername(eq(mainTestUserName))).thenReturn(Optional.of(mainTestUser));
+    when(userRepository.findByUsername(eq(thirdTestUserName))).thenReturn(Optional.of(thirdTestUser));
+
+    UserService userService = new UserService(userRepository);
+    userService.removeFriend(mainTestUserName, thirdTestUserName);
+
+
+    assertEquals(0, userService.getRequiredUser(mainTestUserName).getFriendshipRequests().size());
+    assertEquals(0, userService.getRequiredUser(thirdTestUserName).getFriendshipRequests().size());
+    assertEquals(0, userService.getRequiredUser(mainTestUserName).getFriendshipAddressees().size());
+    assertEquals(0, userService.getRequiredUser(thirdTestUserName).getFriendshipAddressees().size());
+    verify(userRepository, times(1)).save(eq(mainTestUser));
+    verify(userRepository, times(1)).save(eq(thirdTestUser));
+  }
+
   private List<UserWithStatus> getMockUsersMappingFromDb() {
     return List.of(
         new UserWithStatus(
@@ -168,7 +410,7 @@ class UserServiceTest {
             secondTestUser.getCurrency(),
             secondTestUser.getFullname(),
             secondTestUser.getPhotoSmall(),
-            FriendshipStatus.PENDING
+            PENDING
         ),
         new UserWithStatus(
             thirdTestUser.getId(),
@@ -180,4 +422,23 @@ class UserServiceTest {
         )
     );
   }
+
+  private List<UserWithStatus> getMockUserFriendFromDb() {
+    return List.of(
+        new UserWithStatus(
+            thirdTestUser.getId(),
+            thirdTestUser.getUsername(),
+            thirdTestUser.getCurrency(),
+            thirdTestUser.getFullname(),
+            thirdTestUser.getPhotoSmall(),
+            FriendshipStatus.ACCEPTED
+        )
+    );
+  }
+
+  private String bytesToString(byte[] bytes) {
+    if( bytes == null || bytes.length == 0) {return null;}
+    return new String(bytes, StandardCharsets.UTF_8);
+  }
+
 }
